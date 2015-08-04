95fa5ff55d1d59751167cf4e9e566f83485f4aea
Hiroshi Hatake <hatake@clear-code.com>
2015-08-04 12:07:50 +0900

    Support parser plugins

--- lib/fluent/plugin/in_named_pipe.rb
+++ lib/fluent/plugin/in_named_pipe.rb
@@ -25,12 +25,7 @@ module Fluent
         raise ConfigError, "#{e.class}: #{e.message}"
       end
 
-      config['format'] = @format
-      configure_parser(conf)
-    end
-
-    def configure_parser(conf)
-      @parser = TextParser.new
+      @parser = Plugin.new_parser(conf['format'])
       @parser.configure(conf)
     end
 
